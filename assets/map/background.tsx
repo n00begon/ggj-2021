e8de230177d9c6d8e5e5d0ddd5ce2ec04fc953c3
Tim Neumegen <tneumegen@gmail.com>
2021-01-29 20:36:35 +1300

    Tiled Example

---
+++ assets/map/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="background" tilewidth="128" tileheight="128" spacing="4" tilecount="18" columns="9">
+ <image source="../spritesheets/background.png" width="1184" height="260"/>
+</tileset>
