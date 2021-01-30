e77e4a6cc9482f537dd4c8928646a5ed8f989cf2
Tim Neumegen <tneumegen@gmail.com>
2021-01-31 00:23:40 +1300

    Added broken flag

--- src/main/objects/Barrel.ts
+++ src/main/objects/Barrel.ts
@@ -3,21 +3,27 @@ import { Pirate } from "./Pirate";
 export class Barrel {
     public barrel: Phaser.Physics.Arcade.Sprite;
     private scene: Phaser.Scene;
+    private broken = false;
 
     constructor(scene: Phaser.Scene, x: number, y: number, pirateA: Pirate, pirateB: Pirate) {
         this.scene = scene;
         this.barrel = scene.physics.add.sprite(x, y, "sprites", "Barrel-01");
         this.barrel.depth = this.barrel.getBottomCenter().y;
         scene.physics.add.collider(this.barrel, pirateA.getSprite(), () => {
-            console.log("YUM", pirateA);
-            this.barrel.setVisible(false);
-            this.barrel.setActive(false);
+            if (!this.broken) {
+                console.log("YUM", pirateA);
+                this.barrel.setVisible(false);
+                this.barrel.setActive(false);
+                this.broken = true;
+            }
         });
 
         scene.physics.add.collider(this.barrel, pirateB.getSprite(), () => {
-            console.log("YUM", pirateB);
-            this.barrel.setVisible(false);
-            this.barrel.setActive(false);
+            if (!this.broken) {
+                console.log("YUM", pirateB);
+                this.barrel.setVisible(false);
+                this.barrel.setActive(false);
+            }
         });
     }
 }
