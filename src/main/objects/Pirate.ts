ec20fbb88ab14e0e466c88852ff1bd75ee314c0e
stbelousov <int128t@gmail.com>
2021-01-31 17:40:34 +1300

    Add some visibility when we've picked up the barrel

--- src/main/objects/Pirate.ts
+++ src/main/objects/Pirate.ts
@@ -83,7 +83,7 @@ export class Pirate {
 
     public receivedBarrel(barrel: Barrel): void {
         if (barrel.isPuzzlePiece) {
-            MainEventsManager.emit("foundPuzzle", this.player, barrel.puzzleX, barrel.puzzleY);
+            MainEventsManager.emit("foundPuzzle" + this.player, barrel.puzzleX, barrel.puzzleY);
         }
     }
 
