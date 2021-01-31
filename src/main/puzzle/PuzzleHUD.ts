fdc4841da50d44d0107844d026ff4eca2a6d2751
Tim Neumegen <tneumegen@gmail.com>
2021-01-31 17:20:02 +1300

    Show hide map

--- src/main/puzzle/PuzzleHUD.ts
+++ src/main/puzzle/PuzzleHUD.ts
@@ -4,20 +4,45 @@ import { BackgroundManager } from "../BackgroundManager";
 export class PuzzleHUD {
     public x: number;
     public y: number;
-
+    private treasureMap: Phaser.Tilemaps.Tilemap;
     constructor(scene: Phaser.Scene, x: number, y: number, player: number, background: BackgroundManager) {
         this.x = x;
         this.y = y;
-        const treasureMap = background.getTreasureMap(player);
-        treasureMap.getLayer("treasureIsland").tilemapLayer.setPosition(x, y);
-        treasureMap.getLayer("treasureObjects").tilemapLayer.setPosition(x, y);
+        this.treasureMap = background.getTreasureMap(player);
+        this.treasureMap.getLayer("treasureIsland").tilemapLayer.setPosition(x, y);
+        this.treasureMap.getLayer("treasureObjects").tilemapLayer.setPosition(x, y);
+        this.hide();
     }
 
     public hide(): void {
-        console.log("hide");
+        const treasureMapSize = 5;
+        for (let x = 0; x < treasureMapSize; x++) {
+            for (let y = 0; y < treasureMapSize; y++) {
+                const islandTile = this.treasureMap.getLayer("treasureIsland").tilemapLayer.getTileAt(x, y);
+                if (islandTile) {
+                    islandTile.setVisible(false);
+                }
+                const objectTile = this.treasureMap.getLayer("treasureObjects").tilemapLayer.getTileAt(x, y);
+                if (objectTile) {
+                    objectTile.setVisible(false);
+                }
+            }
+        }
     }
 
     public show(): void {
-        console.log("show");
+        const treasureMapSize = 5;
+        for (let x = 0; x < treasureMapSize; x++) {
+            for (let y = 0; y < treasureMapSize; y++) {
+                const islandTile = this.treasureMap.getLayer("treasureIsland").tilemapLayer.getTileAt(x, y);
+                if (islandTile) {
+                    islandTile.setVisible(true);
+                }
+                const objectTile = this.treasureMap.getLayer("treasureObjects").tilemapLayer.getTileAt(x, y);
+                if (objectTile) {
+                    objectTile.setVisible(true);
+                }
+            }
+        }
     }
 }
